503ac1e45fb861a3cbf4468fcf9262f6759de3ec
Arthur Wegner Wippel <arthurwwippel@gmail.com>
2022-06-29 08:56:50 -0300

    Config title and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['@/assets/defaults.css', '@/assets/style.css'],
+  meta: {
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    title: 'Flock - Focus sessions',
+  },
 })
